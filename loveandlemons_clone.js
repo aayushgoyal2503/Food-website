adb3e836a94d8bb85ddf3281fc99759cd56c3937
AAYUSH <aayush.10463@gmail.com>
2024-07-08 09:50:57 +0530

    Food website

---
+++ loveandlemons_clone.js
@@ -0,0 +1,6 @@
+// let a = alert("hello visitors");
+// let b = prompt("good morning!");
+
+let ele = document.getElementsByClassName("bodysection")[0];
+// ele.innerHTML = "<b><u>Hey Yashika!!</u></b>";
+console.log(ele);
